c501826f5ffd103ed91375262c502fdc3ac892a6
Matt Moore <m-c-moore@outlook.com>
2019-07-05 10:31:14 +0100

    Extract postcode from form and submit request

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const port = 3000
-const BusStop = require('./backend/BusStop').BusStop;
+const BusStop = require('/backend/BusStop').BusStop;
 
 app.get('/test', (req, res) => res.send('Hello World!'));
 
--- frontend/departures.js
+++ frontend/departures.js
@@ -1,11 +1,39 @@
-var xhttp = new XMLHttpRequest();
- 
-xhttp.open('GET', '/departureBoards/nw51tl', true);
-xhttp.setRequestHeader('Content-Type', 'application/json');
- 
-xhttp.onload = function() {
-    // Handle response here using e.g. xhttp.status, xhttp.response, xhttp.responseText
-    console.log(xhttp.response);
+function stringify(bus) {
+    var rep = bus.number + " to " + bus.destination + " in " + bus.minutesToArrival.toFixed(0) + " minutes";
+    return rep;
 }
- 
-xhttp.send();
+
+function createResultsHTML(busStopArray) {
+    var resultsText = '<h2>Results</h2>';
+    
+    for (var busStop of busStopArray) {
+        resultsText += '<h3>' + busStop.name + '</h3><ul>';
+        console.log(busStop);
+        for (var bus of busStop.nextBuses) {
+            var busDetails = stringify(bus);
+            resultsText += '<li>' + busDetails +'</li>';
+        }
+        resultsText += '</ul>';
+    }
+    return resultsText;
+}
+
+
+
+function submitPostcode() {
+    var xhttp = new XMLHttpRequest();
+    //var form     = this.form;
+    var postcode = document.forms.postcodeForm[0].value;
+    xhttp.open('GET', '/departureBoards/'+postcode, true);
+    xhttp.setRequestHeader('Content-Type', 'application/json');
+    
+    xhttp.onload = function() {
+        // Handle response here using e.g. xhttp.status, xhttp.response, xhttp.responseText
+        console.log(xhttp.response);
+        var jsonArray = JSON.parse(xhttp.response);
+        var resultsText = createResultsHTML(xhttp.response);
+        document.getElementById("results").innerHTML = resultsText;
+    }
+    
+    xhttp.send();
+}
