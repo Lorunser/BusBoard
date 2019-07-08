a2498c645b0ad8b98e6dd226059b195c4352e202
Lorunser <ltray99@gmail.com>
2019-07-08 12:09:26 +0100

    Edit history to adhere to template

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express()
 const port = 3000
 const BusStop = require('./backend/BusStop').BusStop;
 
+//api
 app.get('/departureBoards/:postcode', async (request, response) => {
     const postcode = request.params.postcode;
 
@@ -15,12 +16,13 @@ app.get('/departureBoards/:postcode', async (request, response) => {
     }
 });
 
+//serve frontend directory
 app.use(express.static('frontend'));
-app.use('/test', express.static('frontend/test.html'));
 
-app.use('/', express.static('frontend/timetable.html'));
+//custom routes
+app.use('/test', express.static('frontend/test.html'));
 app.use('/timetable', express.static('frontend/timetable.html'));
-app.use('/map', express.static('frontend/map.html'));
 app.use('/history', express.static('frontend/history.html'));
 
+//listen
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
